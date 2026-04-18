f9ce2709cd8bcb79b2e47571a6fa1e306895d366
parkdaihee <park827401@naver.com>
2026-04-18 23:25:56 +0900

    dafasdf

--- lib/crypto-server.ts
+++ lib/crypto-server.ts
@@ -1,13 +1,27 @@
-import crypto from 'crypto';
-import forge from 'node-forge';
+import crypto from 'crypto'
+import forge from 'node-forge'
 
 export function randomNonce(bytes = 32) {
-  return crypto.randomBytes(bytes).toString('hex');
+  return crypto.randomBytes(bytes).toString('hex')
 }
 
-export function verifySignature({ message, signatureBase64, certPem }: { message: string; signatureBase64: string; certPem: string; }) {
-  const cert = forge.pki.certificateFromPem(certPem);
-  const md = forge.md.sha256.create();
-  md.update(message, 'utf8');
-  return cert.publicKey.verify(md.digest().bytes(), forge.util.decode64(signatureBase64));
+export function verifySignature({
+  message,
+  signatureBase64,
+  certPem,
+}: {
+  message: string
+  signatureBase64: string
+  certPem: string
+}) {
+  const cert = forge.pki.certificateFromPem(certPem)
+  const publicKey = cert.publicKey as unknown as forge.pki.rsa.PublicKey
+
+  const md = forge.md.sha256.create()
+  md.update(message, 'utf8')
+
+  return publicKey.verify(
+    md.digest().bytes(),
+    forge.util.decode64(signatureBase64),
+  )
 }
